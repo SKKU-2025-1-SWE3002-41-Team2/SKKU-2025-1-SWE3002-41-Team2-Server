8f7578b93890f832487afc6cab10980ea2098200
pushow <qkrehdals55@gmail.com>
2025-06-23 00:34:16 +0900

    summary 수정

--- app/models/chat_session.py
+++ app/models/chat_session.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone, timedelta
 
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import Text
 
 from app.database import Base
 from app.utils.timezone import KST
@@ -13,7 +14,7 @@ class ChatSession(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     userId = Column(Integer, ForeignKey("user.id"), nullable=False)
     name = Column(String(255))
-    summary = Column(String(500),nullable=True)
+    summary = Column(Text,nullable=True)
     createdAt = Column(DateTime, default=lambda: datetime.now(KST))
     modifiedAt = Column(DateTime, default=lambda: datetime.now(KST))
 
